45cad023064158a6839ada38d04de3c7f2f721d4
ProphetLamb <rene.carannante@outlook.de>
2020-05-20 13:41:32 +0200

    Add project files.

---
+++ ColorWindowTitlebar/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestApp
+{
+    class MainWindowViewModel : VMBase
+    {
+        private bool _colorWindowTitlebar = true;
+        public bool ColorWindowTitlebar
+        {
+            get => _colorWindowTitlebar;
+            set => Set(ref _colorWindowTitlebar, value);
+        }
+    }
+}
---
+++ ColorWindowTitlebar/VMBase.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestApp
+{
+    public abstract class VMBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool Set<T>(ref T field, T newValue = default(T), [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
